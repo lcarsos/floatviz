8780359b7478f0216fab9e01969ec6aefd4e5474
lcarsos <lcarsos@gmail.com>
2018-08-18 15:16:12 -0600

    working on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,33 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
+  entry: './src/index.js',
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules',
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
         ],
-    },
+      },
+    ],
+  },
 };
